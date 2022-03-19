3c8cf3bdcf50f4395c502f47ea0b78626e0e1a31
valvic66 <valvic6@gmail.com>
2022-03-19 15:09:36 +0200

    paralax demo

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,9 +4,20 @@ import Container from "@mui/material/Container"
 import "./layout.css"
 import Header from '../components/header';
 
+const backgroundImageUrl = 'https://cdn.dribbble.com/users/211312/screenshots/6007441/media/8b59f94d9ef3e15c2e3ae256bc4aa28d.jpg?compress=1&resize=400x300';
+
 const Layout = ({ children }) => {
   return (
-    <div className="wrapper" style={{backgroundColor: '#000'}}>
+    <div
+      className="wrapper"
+      style={{
+        backgroundImage: `url(${backgroundImageUrl})`,
+        backgroundAttachment: 'fixed',
+        // backgroundPosition: 'center',
+        // backgroundRepeat: 'no-repeat',
+        // backgroundSize: 'cover'
+      }}
+    >
       <Header siteTitle="Portfolio Site" />
       <Container maxWidth="md">
         <main>{children}</main>
